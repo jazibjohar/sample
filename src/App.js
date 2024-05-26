5284fe3371222d7a20536865831084d29285e161
Syed Muhammad Baligh ur Rehman <baligh@kiddom.co>
2024-05-25 18:26:14 -0700

    ✨ (App.js): Introduce counter functionality with operations (+, -, X, /, Reset) ♻️ (App.js): Refactor state variable name from 'state' to 'count' for better readability ♻️ (button.jsx): Refactor Button component to receive onClick function as prop, remove internal state and callback 💡 (App.js, button.jsx): Add useEffect hooks to log when counter and Button component are rendered

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import './App.css';
 
+import { useCallback, useEffect, useState } from 'react';
+
 import { Button } from './components/button';
-import { useState } from 'react';
 
 function App() {
-  const [state, setState] = useState();
-  const onChange = (e) => {
-    setState(e.target.value);
-  };
+  const [count, setCount] = useState(0);
+  const updateCount = useCallback(
+    (operation) => {
+      if (operation === '+') {
+        setCount(count + 1);
+      }
+      if (operation === '-') {
+        setCount(count - 1);
+      }
+      if (operation === 'X') {
+        setCount(count * 2);
+      }
+      if (operation === '/') {
+        setCount(count / 2);
+      }
+    },
+    [count, setCount]
+  );
+  const operations = [
+    {
+      buttonName: '+',
+      onClick: () => updateCount('+'),
+    },
+    {
+      buttonName: '-',
+      onClick: () => updateCount('-'),
+    },
+    {
+      buttonName: 'Reset',
+      onClick: () => setCount(0),
+    },
+    {
+      buttonName: 'X',
+      onClick: () => updateCount(`X`),
+    },
+    {
+      buttonName: '/',
+      onClick: () => updateCount('/'),
+    },
+  ];
+  useEffect(() => {
+    console.log('counter rendered');
+  }, [count]);
   return (
     <div className='App'>
       <header className='App-header'></header>
-      <h1>Hello World</h1>
-      <input onChange={onChange} type='text' />
-      <Button buttonName={state ?? 'No Input'} />
-      <Button buttonName='Button 2' />
-      <Button buttonName='Button X' />
-      <Button buttonName='Button A' />
-      <Button buttonName='Button c' />
+      <h1>Count: {count}</h1>
+      {operations.map((operation, index) => (
+        <Button key={`${index}-${count}`} {...operation} />
+      ))}
     </div>
   );
 }
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,21 +1,12 @@
-import React, { useCallback, useState } from 'react';
+import React, { useEffect } from 'react';
 
-export const Button = ({ buttonName }) => {
-  const [count, setCount] = useState(100);
-  const testCallback = useCallback(() => {
-    console.log('Loggin from here');
-    console.log(count);
-  }, [count]);
-
-  const handleClick = () => {
-    setCount(count + 1);
-    testCallback();
-  };
-
+export const Button = ({ buttonName, onClick }) => {
+  useEffect(() => {
+    console.log('on mount');
+  }, []);
   return (
     <div>
-      <label>Counter: {count} </label>
-      <button onClick={handleClick}>{buttonName}</button>
+      <button onClick={onClick}>{buttonName}</button>
     </div>
   );
 };
